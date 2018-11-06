eb647b512a9e838a9865d9978e1736e8f8f75925
Simon Amtoft Pedersen <39790702+Lalelilu@users.noreply.github.com>
2018-11-06 12:54:11 +0100

    comments

--- RISCVsimulator.java
+++ RISCVsimulator.java
@@ -1,5 +1,6 @@
-/*
+/* File: RISCVsimulator.java
  * Authors: Simon Amtoft Pedersen & Marc Sun Bøg
+ * The following file simulates the RISC-V instructions set from a binary input file 
  */
 import java.io.*;
 
@@ -8,12 +9,11 @@ public class RISCVsimulator {
 
     public static void main(String[] args) throws IOException {
         instField inst = new instField();
-        pc = 0;
-        // Start reading from binary file
-        String path = "addlarge.bin";
-        int[] program = getInstructions(path);
+        pc = 0;                                 // Program counter 
+        String path = "addlarge.bin";           // Path of binary file 
+        int[] program = getInstructions(path);  // Read all instructions from binary file
     }
-    
+     
     // Returns array of 32-bit instructions from input file given in 'path' 
     private static int[] getInstructions(String path) throws IOException {
         File f = new File(path);
@@ -29,7 +29,4 @@ public class RISCVsimulator {
     private static int addi() {
         return 0;
     }
-
-
-
 }
