87011ca149313946a8f86feaf31b3c898ec872a6
Marc <17163097+MarcMarabou@users.noreply.github.com>
2018-11-09 14:20:30 +0100

    Added load and store instructions, refactoring

--- RISCVsimulator.java
+++ RISCVsimulator.java
@@ -8,6 +8,7 @@ public class RISCVsimulator {
     private static int pc;
     private static int[] program;
     private static int[] reg;
+    private static int[] memory;
 
     public static void main(String[] args) throws IOException {
         String test = "add_large";
@@ -16,16 +17,12 @@ public class RISCVsimulator {
         pc = 0;                                 // Program counter
         reg = new int[32];                      // Define register to be array of 32 elements (x0 to x31)
         program = getInstructions(path);        // Read all instructions from binary file
-
+        memory = new int[128*1000*1000/4];      // 128MB of memory allocated.
+        reg[2] = 128*1000*1000 - 4;             // Initialize sp to last word in memory.
         while (pc < program.length) {
-            int prevPc = pc;
-            String str = String.format("Opcode: %02x Rd: %02x Rs1: %02x Rs2: %02x Funct3: %02x Funct7: %02x",
-                    instHelper.getOpcode(program[pc]), instHelper.getRd(program[pc]), instHelper.getRs1(program[pc]),
-                    instHelper.getRs2(program[pc]), instHelper.getFunct3(program[pc]), instHelper.getFunct7(program[pc]));
-            System.out.println(str);
             executeInstruction(program[pc]);
-            System.out.println("x"+instHelper.getRd(program[prevPc])+": " + reg[instHelper.getRd(program[prevPc])]);
         }
+        System.out.println("---");
         endOfProgram(reg);
         compareResults(dir+test+".res", "./output.bin");
     }
@@ -95,23 +92,27 @@ public class RISCVsimulator {
     
     // JAL and JALR
     private static void jumpTypes(int instruction, int opcode){
-        int Rd = instHelper.getRd(instruction), Rs, Imm;
+        int Rd = instHelper.getRd(instruction), Rs = 0, Imm = 0;
+        String type = "Unrecognized Opcode";
         switch(opcode){
             case 0b1101111: //JAL
+                type = "JAL";
                 Imm = instHelper.getImmJ(instruction);
                 reg[Rd] = (pc+1)*4; // Store address of next instruction in bytes
                 pc += Imm/4;
                 return;
             case 0b1100111: //JALR
+                type = "JALR";
                 Rs = instHelper.getRs1(instruction);
                 Imm = instHelper.getImmI(instruction);
                 reg[Rd] = (pc+1)*4;
                 pc = ((reg[Rs] + Imm) & 0xFFFFFFFE)/4;
                 return;
         }
+        System.out.println(String.format("%s x%02d 0x%x", type, Rd, Imm));
         pc++;
     }
-    
+
     // R-type instructions: ADD / SUB / SLL / SLT / SLTU / XOR / SRL / SRA / OR / AND
     private static void rType(int instruction) {
         // Get fields
@@ -120,71 +121,100 @@ public class RISCVsimulator {
         int Rd = instHelper.getRd(instruction);
         int Rs1 = instHelper.getRs1(instruction);
         int Rs2 = instHelper.getRs2(instruction);
-
+        String type = "Unrecognized Opcode";
         switch(funct3){
             case 0b000: // ADD / SUB
                 switch(funct7){
                     case 0b0000000: // ADD
+                        type = "ADD";
                         reg[Rd] = reg[Rs1] + reg[Rs2];
                         break;
                     case 0b0100000: // SUB
+                        type = "SUB";
                         reg[Rd] = reg[Rs1] - reg[Rs2];
                         break;
                 }
                 break;
             case 0b001: // SLL
+                type = "SLL";
                 reg[Rd] = reg[Rs1] << reg[Rs2];
                 break;
             case 0b010: // SLT
+                type = "SLT";
                 if (reg[Rs1] < reg[Rs2])
                     reg[Rd] = 1;
                 else
                     reg[Rd] = 0;
                 break;
             case 0b011: // SLTU
+                type = "SLTU";
                 if (Integer.toUnsignedLong(reg[Rs1]) < Integer.toUnsignedLong(reg[Rs2]))
                     reg[Rd] = 1;
                 else
                     reg[Rd] = 0;
                 break;
             case 0b100: // XOR
+                type = "XOR";
                 reg[Rd] = reg[Rs1] ^ reg[Rs2];
                 break;
             case 0b101: // SRL / SRA
                 switch(funct7){
                     case 0b0000000: // SRL
+                        type = "SRL";
                         reg[Rd] = reg[Rs1] >>> reg[Rs2];
                         break;
                     case 0b0100000: // SRA
+                        type = "SRA";
                         reg[Rd] = reg[Rs1] >> reg[Rs2];
                         break;
                 }
                 break;
             case 0b110: // OR
+                type = "OR";
                 reg[Rd] = reg[Rs1] | reg[Rs2];
                 break;
             case 0b111: // AND
+                type = "AND";
                 reg[Rd] = reg[Rs1] & reg[Rs2];
                 break;
         }
+        System.out.println(String.format("%s x%02d x%02d x%02d", type, Rd, Rs1, Rs2));
         pc++;
     }
 
     // I-type load instructions: LB / LH / LW / LBU / LHU
     private static void iTypeLoad(int instruction) {
         int funct3 = instHelper.getFunct3(instruction); // Gets the funct3 field of instruction
+        int Rd = instHelper.getRd(instruction);
+        int Rs1 = instHelper.getRs1(instruction);
+        int ImmI = instHelper.getImmI(instruction);
+        int addr = reg[Rs1] + ImmI;
+        String type = "Unrecognized Opcode";
         switch(funct3){
+            // This assumes properly aligned addresses in all scenarios. LH / LW wont work properly if misaligned.
             case 0b000: // LB
+                type = "LB";
+                reg[Rd] = (byte) ((memory[addr >> 2] >> ((addr & 0x3) << 3)) & 0xFF); //Get the entire word by dividing addr by 4. Find byte-offset by (addr & 0x3) and multiply by 8 for needed bitshift value.
                 break;
             case 0b001: // LH
+                type = "LH";
+                reg[Rd] = (short) ((memory[addr >> 2] >> ((addr & 0x3) << 3)) & 0xFFFF);
+                System.out.println((addr >> 2) + " " + (addr&0x2));
                 break;
             case 0b010: // LW
+                type ="LW";
+                reg[Rd] = memory[addr >> 2]; //Load entire word, does not work with misaligned address.
                 break;
             case 0b100: // LBU
+                type = "LBU";
+                reg[Rd] = (memory[addr >> 2] >> ((addr & 0x3) << 3)) & 0xFF; //Remove sign bits
                 break;
             case 0b101: // LHU
+                type = "LHU";
+                reg[Rd] = (memory[addr >> 2] >> ((addr & 0x3) << 3)) & 0xFFFF;
                 break;
         }
+        System.out.println(String.format("%s x%d %d(x%d)", type, Rd, ImmI, Rs1));
         pc++;
     }
 
@@ -195,34 +225,40 @@ public class RISCVsimulator {
         int Rs1 = instHelper.getRs1(instruction);
         int ImmI = instHelper.getImmI(instruction);
         int funct3 = instHelper.getFunct3(instruction);
-
+        String type = "Unrecognized Opcode";
         switch(funct3){
             case 0b000: // ADDI
+                type = "ADDI";
                 reg[Rd] = reg[Rs1] + ImmI;
                 break;
             case 0b010: // SLTI
+                type = "SLTI";
                 if(reg[Rs1] < ImmI)
                     reg[Rd] = 1;
                 else
                     reg[Rd] = 0;
                 break;
             case 0b011: // SLTIU
-                // REG RS1 SKAL VÆRE UNSIGNED???
+                type = "SLTIU";
                 if(Integer.toUnsignedLong(reg[Rs1]) < Integer.toUnsignedLong(ImmI))
                     reg[Rd] = 1;
                 else
                     reg[Rd] = 0;
                 break;
             case 0b100: // XORI
+                type = "XORI";
                 reg[Rd] = reg[Rs1] ^ ImmI;
                 break;
             case 0b110: // ORI
+                type = "ORI";
                 reg[Rd] = reg[Rs1] | ImmI;
                 break;
             case 0b111: // ANDI
+                type = "ANDI";
                 reg[Rd] = reg[Rs1] & ImmI;
                 break;
             case 0b001: // SLLI
+                type = "SLLI";
                 reg[Rd] = reg[Rs1] << ImmI;
                 break;
             case 0b101: // SRLI / SRAI
@@ -230,14 +266,17 @@ public class RISCVsimulator {
                 int funct7 = instHelper.getFunct7(instruction);
                 switch(funct7){
                     case 0b0000000: // SRLI
+                        type = "SRLI";
                         reg[Rd] = reg[Rs1] >>> ShiftAmt;
                         break;
                     case 0b0100000: // SRAI
+                        type = "SRAI";
                         reg[Rd] = reg[Rs1] >> ShiftAmt;
                         break;
                 }
                 break;
         }
+        System.out.println(String.format("%s x%d x%d %d", type, Rd, Rs1, ImmI));
         pc++;
     }
 
@@ -262,22 +301,29 @@ public class RISCVsimulator {
                 ImmI = instHelper.getImmI(instruction);
                 switch(ImmI){
                     case 0b000000000000: // ECALL
-                        switch (reg[10) {
+                        System.out.println("ECALL "+ reg[10]);
+                        switch (reg[10]) {
                             case 1:     // print_int
+                                System.out.print(reg[11]);
+                                break;
                             case 4:     // print_string
+                                //System.out.print(memory[reg[11]]);
+                                break;
                             case 9:     // sbrk
                                 // not sure if we can do this?
+                                break;
                             case 10:    // exit
-                               pc = program.length; // Sets program counter to end of program, to program loop
-                               return;              // Exits 'iTypeStatus' function and returns to loop. 
+                                pc = program.length; // Sets program counter to end of program, to program loop
+                                return;              // Exits 'iTypeStatus' function and returns to loop.
                             case 11:    // print_character
-                                char a1 = (char) reg[11];
+                                System.out.print((char) reg[11]);
+                                break;
                             case 17:    // exit2
                                 pc = program.length;
-                                System.out.println("a1 = " reg[11]); // Prints a1 (should be return?)
+                                System.out.println("Return code: " + reg[11]); // Prints a1 (should be return?)
                                 return;
-                            default: 
-                                System.out.println("ECALL" + reg[10] + " not implemented");
+                            default:
+                                System.out.println("ECALL " + reg[10] + " not implemented");
                                 break;
                         }
                     case 0b000000000001: // EBREAK
@@ -303,14 +349,29 @@ public class RISCVsimulator {
     // S-type instructions: SB / SH / SW
     private static void sType(int instruction) {
         int funct3 = instHelper.getFunct3(instruction); // Gets the funct3 field of instruction
+        int Rs1 = instHelper.getRs1(instruction);
+        int Rs2 = instHelper.getRs2(instruction);
+        int ImmS = instHelper.getImmS(instruction);
+        int addr = reg[Rs1] + ImmS;
+        String type = "Unrecognized Opcode";
         switch(funct3){
             case 0b000: // SB
+                type = "SB";
+                memory[addr >> 2] &= ~(0xFF << ((addr & 0x3) << 3)); //Same logic as LB instruction. We clear the byte at the word-address + offset.
+                memory[addr >> 2] |= (reg[Rs2] & 0xFF) << ((addr & 0x3) << 3); //Store byte from reg[Rs2] at word-address + offset
                 break;
             case 0b001: // SH
+                type = "SH";
+                memory[addr >> 2] &= ~(0xFFFF << ((addr & 0x2) << 3)); //Same logic as LH instruction. We clear the half-word at the word-address + offset.
+                memory[addr >> 2] |= (reg[Rs2] & 0xFFFF) << ((addr & 0x2) << 3); //Store half-word from reg[Rs2] at word-address + offset
                 break;
             case 0b010: // SW
+                type = "SW";
+                memory[addr >> 2] = reg[Rs2];
                 break;
         }
+        System.out.println(String.format("%s x%d %d(x%d)", type, Rs2, ImmS, Rs1));
+        pc++;
     }
 
     // B-type instructions: BEQ / BNE / BLT / BGE / BLTU / BGEU
@@ -318,43 +379,56 @@ public class RISCVsimulator {
         int funct3 = instHelper.getFunct3(instruction);
         int Rs1 = instHelper.getRs1(instruction);
         int Rs2 = instHelper.getRs2(instruction);
-        int ImmB = instHelper.getImmB(instruction) / 4; //We're counting in words instead of bytes
-
+        int ImmB = instHelper.getImmB(instruction) >> 2; //We're counting in words instead of bytes
+        String type = "Unrecognized Opcode";
         switch(funct3){
             case 0b000: // BEQ
-                pc += (reg[Rs1] == reg[Rs2]) ? ImmB : 1;
+                type = "BEQ";
+                pc += (reg[Rs1] == reg[Rs2])? ImmB : 1;
                 break;
             case 0b001: // BNE
-                pc += (reg[Rs1] != reg[Rs2]) ? ImmB : 1;
+                type = "BNE";
+                pc += (reg[Rs1] != reg[Rs2])? ImmB : 1;
                 break;
             case 0b100: // BLT
-                pc += (reg[Rs1] < reg[Rs2]) ? ImmB : 1;
+                type = "BLT";
+                pc += (reg[Rs1] < reg[Rs2])? ImmB : 1;
                 break;
             case 0b101: // BGE
-                pc += (reg[Rs1] >= reg[Rs2]) ? ImmB : 1;
+                type = "BGE";
+                pc += (reg[Rs1] >= reg[Rs2])? ImmB : 1;
                 break;
             case 0b110: //BLTU
-                pc += (Integer.toUnsignedLong(reg[Rs1]) < Integer.toUnsignedLong(reg[Rs2])) ? ImmB : 1;
+                type = "BLTU";
+                pc += (Integer.toUnsignedLong(reg[Rs1]) < Integer.toUnsignedLong(reg[Rs2]))? ImmB : 1;
                 break;
             case 0b111: //BLGEU
-                pc += (Integer.toUnsignedLong(reg[Rs1]) >= Integer.toUnsignedLong(reg[Rs2])) ? ImmB : 1;
+                type = "BLGEU";
+                pc += (Integer.toUnsignedLong(reg[Rs1]) >= Integer.toUnsignedLong(reg[Rs2]))? ImmB : 1;
                 break;
         }
+        System.out.println(String.format("%s x%d x%d %d", type, Rs1, Rs2, ImmB<<2));
     }
-    
+
     // U-type instructions: LUI / AUIPC
     private static void uType(int instruction, int opcode){
         int Rd = instHelper.getRd(instruction);
         int ImmU = instHelper.getImmU(instruction);
+        String type = "Unrecognized Opcode";
         switch(opcode){
             case 0b0010111: // AUIPC
-                reg[Rd] = pc*4; // Compensate for counting in 4 byte words
+                type = "AUIPC";
+                reg[Rd] = pc*4 + ImmU; // As we count in 4 byte words
+                break;
             case 0b0110111: // LUI
-                reg[Rd] += ImmU;
+                type = "LUI";
+                reg[Rd] = ImmU;
                 break;
         }
+        System.out.println(String.format("%s x%d %d", type, Rd, ImmU>>>12));
         pc++;
     }
+
     
     // Prints the contents of the registers x0 to x31
     private static void printRegisterContent(int reg[]) {
