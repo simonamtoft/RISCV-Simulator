895fdca62f8df85f35425bca8e49a785fd72657b
Simon Amtoft Pedersen <39790702+Lalelilu@users.noreply.github.com>
2018-11-14 22:15:32 +0100

    intro comment

--- CPU.java
+++ CPU.java
@@ -1,6 +1,11 @@
+/* File: CPU.java
+ * Authors: Marc Sun Bog & Simon Amtoft Pedersen
+ *
+ * The following file handles Instructions
+ */
 package RISCVSimulator;
 
 public class CPU {
-    int pc = 0;
-    int[] reg = new int[32];
+    int pc = 0;                 // Program counter
+    int[] reg = new int[32];    // RISC-V registers x0 to x31
 }
