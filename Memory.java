134d8cc0533334fe62560ea4d9b976fdce604bd4
Simon Amtoft Pedersen <39790702+Lalelilu@users.noreply.github.com>
2018-11-14 22:24:13 +0100

    change names, added intro comment

--- Memory.java
+++ Memory.java
@@ -1,41 +1,46 @@
+/* File: Main.java
+ * Authors: Marc Sun Bog & Simon Amtoft Pedersen
+ *
+ * The following file has a memory byte array and handles input and read from the array
+ */
 package RISCVSimulator;
 
 public class Memory {
     private byte[] memory;
 
-    public Memory(int MEMORY_SIZE_IN_BYTES){
-        this.memory = new byte[MEMORY_SIZE_IN_BYTES];
+    public Memory(int byteSize){
+        memory = new byte[byteSize];
     }
 
-    public void store_byte_to_mem(int addr, byte data){
+    public void storeByte (int addr, byte data) {
         memory[addr] = data;
     }
 
-    public void store_halfword_to_mem(int addr, short data){
+    public void storeHalfWord(int addr, short data) {
         memory[addr]    = (byte) (data & 0x00FF);
         memory[addr+1]  = (byte) (data &0xFF00);
     }
 
-    public void store_word_to_mem(int addr, int data){
+    public void storeWord(int addr, int data) {
         memory[addr]    = (byte) (data & 0x000000FF);
         memory[addr+1]  = (byte) (data & 0x0000FF00);
         memory[addr+2]  = (byte) (data & 0x00FF0000);
         memory[addr+3]  = (byte) (data & 0xFF000000);
     }
 
-    public byte get_byte_from_mem(int addr){
+    public byte getByte (int addr) {
         return memory[addr];
     }
 
-    public short get_halfword_from_mem(int addr){
+    public short getHalfWord(int addr){
         return (short) ((memory[addr+1] << 8) | (memory[addr]));
     }
 
-    public int get_word_from_mem(int addr){
+    public int getWord(int addr){
         return ((memory[addr+3] << 8) | (memory[addr+2] << 16) | (memory[addr+1] << 8) | (memory[addr]));
     }
 
-    public String get_string_from_mem(int addr){
+    public String toString(int addr){
         String returnValue = "";
         int i = 0;
         while(memory[addr+i] != 0){
@@ -44,7 +49,8 @@ public class Memory {
         return returnValue;
     }
 
-    public byte[] getMemory() {
+    public byte[] get() {
         return memory;
     }
+
 }
