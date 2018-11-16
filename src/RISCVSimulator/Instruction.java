b81752a24d28a663f4e113e70c710f5e2fde7477
Marc <17163097+MarcMarabou@users.noreply.github.com>
2018-11-16 14:24:20 +0100

    Fixed wrong immS and immB

--- src/RISCVSimulator/Instruction.java
+++ src/RISCVSimulator/Instruction.java
@@ -19,7 +19,7 @@ public class Instruction {
         this.rs1 = (instruction >>15) & 0x1F;    // Returns bits 19 to 15
         this.rs2 = (instruction>>20) & 0x1F;     // Returns bits 24 to 20
         this.immI = (instruction>>20);           // Returns bits 31 to 20
-        this.immS = rd | (funct7<<5); // Returns bits 31 to 25 and 11 to 7
+        this.immS = (((instruction >> 20) & 0xFFFFFFE0) | ((instruction >>> 7) & 0x0000001F)); // Returns bits 31 to 25 and 11 to 7
         this.immB = getImmB(instruction);
         this.immU = instruction & 0xFFFFF000;
         this.immJ = getImmJ(instruction);
@@ -247,13 +247,14 @@ public class Instruction {
     }
 
     private int getImmB (int instruction) {
-        int b11 = (instruction>>7) & 0x1;       // 11'th bit of immediate (7th bit of instruction)
-        int b1to4 = (instruction>>8) & 0xF;     // Bits 1 to 4 of immediate (8 to 11 of instruction)
-        int b5to10 = (instruction>>25) & 0x1F;  // Bits 5 to 10 of immediate (25 to 30 of instruction)
-        int b12 = (instruction>>31) & 0x1;      // Bit 12 of immediate (MSB of instruction)
+        //int b11 = (instruction>>7) & 0x1;       // 11'th bit of immediate (7th bit of instruction)
+        //int b1to4 = (instruction>>8) & 0xF;     // Bits 1 to 4 of immediate (8 to 11 of instruction)
+        //int b5to10 = (instruction>>25) & 0x1F;  // Bits 5 to 10 of immediate (25 to 30 of instruction)
+        //int b12 = (instruction>>31) & 0x1;      // Bit 12 of immediate (MSB of instruction)
 
         // Returns bits in the order: imm[12|10:5|4:1|11]
-        return b11 << 11 | b1to4 << 1 | b5to10 << 5 | b12 << 12;
+        //return b11 << 11 | b1to4 << 1 | b5to10 << 5 | b12 << 12;
+        return (((((instruction >> 20) & 0xFFFFFFE0) | ((instruction >>> 7) & 0x0000001F)) & 0xFFFFF7FE) | ((   (((instruction >> 20) & 0xFFFFFFE0) | ((instruction >>> 7) & 0x0000001F)) & 0x00000001) << 11));
     }
 
     private int getImmJ(int instruction) {
