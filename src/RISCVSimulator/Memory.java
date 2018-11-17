b61b8e1dcf15474e1d7f397b99133563e8f05037
Simon Amtoft Pedersen <39790702+Lalelilu@users.noreply.github.com>
2018-11-17 15:02:30 +0100

    comments

--- src/RISCVSimulator/Memory.java
+++ src/RISCVSimulator/Memory.java
@@ -4,52 +4,55 @@
  * The following file has a memory byte array and handles input and read from the array
  */
 package RISCVSimulator;
-
 import java.nio.charset.StandardCharsets;
 
-class Memory {
+public class Memory {
     private byte[] memory;
 
+    /**
+     * Constructor for Memory
+     * Initializes as a byte array of size given by argument
+     */
     Memory(int MEMORY_SIZE_IN_BYTES){
         memory = new byte[MEMORY_SIZE_IN_BYTES];
     }
 
-    void storeByte(int addr, int data){
+    // Stores a single byte in the memory array
+    void storeByte (int addr, int data) {
         memory[addr] = (byte) (data & 0xFF);
     }
 
-    void storeHalfword(int addr, int data){
-        memory[addr]    = (byte) ((data & 0x00FF));
-        memory[addr+1]  = (byte) ((data & 0xFF00) >>> 8);
+    // Stores a half word in the memory array
+    void storeHalfWord(int addr, short data) {
+        memory[addr]    = (byte) (data & 0x00FF);
+        memory[addr+1]  = (byte) (data &0xFF00);
     }
 
-    void storeWord(int addr, int data){
+    // Stores a word in the memory array
+    void storeWord(int addr, int data) {
         memory[addr]    = (byte) ((data & 0x000000FF));
         memory[addr+1]  = (byte) ((data & 0x0000FF00) >>> 8);
         memory[addr+2]  = (byte) ((data & 0x00FF0000) >>> 16);
         memory[addr+3]  = (byte) ((data & 0xFF000000) >>> 24);
     }
 
-    public void storeString(int addr, String data){
-        byte[] str = data.getBytes(StandardCharsets.US_ASCII);
-        for(int i = 0; i < str.length; i++){
-            memory[addr+i] = str[i];
-        }
-    }
-
-    int getByte(int addr){
+    // Returns the byte in the memory given by the address.
+    public byte getByte (int addr) {
         return memory[addr];
     }
 
+    // Returns half word from memory array given by address
     int getHalfword(int addr){
         return (getByte(addr+1) << 8) | (getByte(addr) & 0xFF);
     }
 
+    // Returns word from memory array given by address
     int getWord(int addr){
         return (getHalfword(addr+2) << 16) | (getHalfword(addr) & 0xFFFF);
     }
 
-    public String getString(int addr){
+    // Returns string starting at the address given and ends when next memory address is zero.
+    String getString(int addr){
         String returnValue = "";
         int i = 0;
         while(memory[addr+i] != 0){
@@ -58,6 +61,7 @@ class Memory {
         return returnValue;
     }
 
+    // Returns the memory array
     byte[] getMemory() {
         return memory;
     }
