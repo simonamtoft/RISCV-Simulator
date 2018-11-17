7ecf406df89067d50ed83e44d982ceef1b8fab7c
Simon Amtoft Pedersen <39790702+Lalelilu@users.noreply.github.com>
2018-11-17 14:42:16 +0100

    comments

--- src/RISCVSimulator/CPU.java
+++ src/RISCVSimulator/CPU.java
@@ -6,32 +6,44 @@
 
 package RISCVSimulator;
 
-class CPU {
-    int pc = 0;
-    int prevPc;
-    int[] reg = new int[32];
-    private Memory memory;
-    private Instruction[] program;
 
-    CPU(Memory memory, Instruction[] program){
-        this.memory = memory;
+public class CPU {
+    int pc = 0;                     // Program counter
+    int prevPc;                     // Previous pc
+    static int[] reg = new int[32]; // RISC-V registers x0 to x31
+    public Instruction[] program;   // Array of all program instructions
+    private Memory memory;          // Memory byte array
+
+    /**
+     * Initializes a CPU with memory 'mem' and the instruction array 'program'
+     * and sp (x2) to point at last memory address.
+     */
+    public CPU(Memory mem, Instruction[] program) {
+        this.memory = mem;
         this.program = program;
         reg[2] = memory.getMemory().length - 1;
     }
 
-    void executeInstruction() {
+    /**
+     * Executes one instruction given by the instruction array program at index given by the program counter
+     * Uses the opcode field of the instruction to determine which type of instruction it is and call that method.
+     */
+    public void executeInstruction(){
         prevPc = pc;
         Instruction instr = program[pc];
-        switch (instr.opcode) {
+        switch(instr.opcode){
             // R-type instructions
             case 0b0110011: // ADD / SUB / SLL / SLT / SLTU / XOR / SRL / SRA / OR / AND
                 rType(instr);
                 break;
+
             // J-type instruction
             case 0b1101111: //JAL
+                jumpType(instr);
+                break;
             // I-type instructions
             case 0b1100111: // JALR
-                jumpTypes(instr);
+                jumpType(instr);
                 break;
             case 0b0000011: // LB / LH / LW / LBU / LHU
                 iTypeLoad(instr);
@@ -63,23 +75,29 @@ class CPU {
                 break;
         }
         reg[0] = 0; // x0 must always be 0
-        System.out.println(instr.assemblyString);
     }
 
-    private void jumpTypes(Instruction inst){
+    /**
+     * Handles execution of following instructions
+     * JAL / JALR
+     */
+    private void jumpType(Instruction inst){
         switch(inst.opcode){
             case 0b1101111: //JAL
-                reg[inst.rd] = (pc+1) << 2; // Store address of next instruction in bytes
-                pc += inst.immJ >> 2;
+                reg[inst.rd] = (pc+1)<<2; // Store address of next instruction in bytes
+                pc += inst.immJ>>2;
                 break;
             case 0b1100111: //JALR
-                reg[inst.rd] = (pc+1) << 2;
-                pc = ((reg[inst.rs1] + inst.immI) & 0xFFFFFFFE) >> 2;
+                reg[inst.rd] = (pc+1)<<2;
+                pc = ((reg[inst.rs1] + inst.immI) & 0xFFFFFFFE)>>2;
                 break;
         }
-
     }
 
+    /**
+     * Handles execution of r-Type instructions:
+     * ADD / SUB / SLL / SLT / SLTU / XOR / SRL / SRA / OR / AND
+     */
     private void rType(Instruction inst) {
         switch(inst.funct3){
             case 0b000: // ADD / SUB
@@ -130,14 +148,20 @@ class CPU {
         pc++;
     }
 
+    /**
+     * Handles execution of i-Type load instructions:
+     * LB / LH / LW / LBU / LHU
+     */
     private void iTypeLoad(Instruction inst) {
-        int addr = reg[inst.rs1] + inst.immI;
+        int addr = reg[inst.rs1] + inst.immI; // Byte address
+
         switch(inst.funct3){
+            // This assumes properly aligned addresses in all scenarios. LH / LW wont work properly if misaligned.
             case 0b000: // LB
                 reg[inst.rd] = memory.getByte(addr);
                 break;
             case 0b001: // LH
-                reg[inst.rd] = memory.getHalfword(addr);
+                reg[inst.rd] = memory.getHalfWord(addr);
                 break;
             case 0b010: // LW
                 reg[inst.rd] = memory.getWord(addr);
@@ -146,12 +170,18 @@ class CPU {
                 reg[inst.rd] = memory.getByte(addr) & 0xFF; //Remove sign bits
                 break;
             case 0b101: // LHU
-                reg[inst.rd] = memory.getHalfword(addr) & 0xFFFF;
+                reg[inst.rd] = memory.getHalfWord(addr) & 0xFFFF;
+                break;
+            default:
                 break;
         }
         pc++;
     }
 
+    /**
+     * Handles execution of I-type integer instructions:
+     * ADDI / SLTI / SLTIU / XORI / ORI / ANDI / SLLI / SRLI / SRAI
+     */
     private void iTypeInteger(Instruction inst) {
         switch(inst.funct3){
             case 0b000: // ADDI
@@ -196,6 +226,11 @@ class CPU {
         pc++;
     }
 
+
+    /**
+     * Handles the I-type fence instructions:
+     * FENCE / FENCE.I
+     */
     private void iTypeFence(Instruction inst) {
         switch(inst.funct3){
             case 0b000: // FENCE
@@ -206,6 +241,10 @@ class CPU {
         pc++;
     }
 
+    /**
+     * Handles execution of i-Type ECALL and status instructions:
+     * ECALL / EBREAK / CSRRW / CSRRS / CSRRC / CSRRWI / CSRRSI / CSRRCI
+     */
     private void iTypeStatus(Instruction inst) {
         switch(inst.funct3){
             case 0b000: // ECALL / EBREAK
@@ -225,11 +264,11 @@ class CPU {
                                 pc = program.length; // Sets program counter to end of program, to program loop
                                 return;              // Exits 'iTypeStatus' function and returns to loop.
                             case 11:    // print_character
-                                // System.out.print((char) reg[11]);
+                                System.out.print((char) reg[11]);
                                 break;
                             case 17:    // exit2
                                 pc = program.length;
-                                // System.out.println("Return code: " + reg[11]); // Prints a1 (should be return?)
+                                //System.out.println("Return code: " + reg[11]); // Prints a1 (should be return?)
                                 return;
                             default:
                                 System.out.println("ECALL " + reg[10] + " not implemented");
@@ -255,14 +294,18 @@ class CPU {
         pc++;
     }
 
+    /**
+     * Handles the S-type instructions:
+     * SB / SH / SW
+     */
     private void sType(Instruction inst) {
         int addr = reg[inst.rs1] + inst.immS;
         switch(inst.funct3){
             case 0b000: // SB
-                memory.storeByte(addr, reg[inst.rs2]);
+                memory.storeByte(addr,(byte) reg[inst.rs2]);
                 break;
             case 0b001: // SH
-                memory.storeHalfword(addr, reg[inst.rs2]);
+                memory.storeHalfWord(addr, (short) reg[inst.rs2]);
                 break;
             case 0b010: // SW
                 memory.storeWord(addr, reg[inst.rs2]);
@@ -271,6 +314,10 @@ class CPU {
         pc++;
     }
 
+    /**
+     * Handles the B-type instructions:
+     * BEQ / BNE / BLT / BGE / BLTU / BGEU
+     */
     private void bType(Instruction inst) {
         int ImmB = inst.immB >> 2; //We're counting in words instead of bytes
         switch(inst.funct3){
@@ -295,6 +342,10 @@ class CPU {
         }
     }
 
+    /**
+     * Handles the U-type instructions:
+     * LUI / AUIPC
+     */
     private void uType(Instruction inst){
         switch(inst.opcode){
             case 0b0010111: // AUIPC
