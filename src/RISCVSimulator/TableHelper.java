a44eea46202a2c7f98c209bb6d10f983baa38461
Marc <17163097+MarcMarabou@users.noreply.github.com>
2018-11-17 13:37:06 +0100

    Update and rename tableHelper.java to TableHelper.java

---
+++ src/RISCVSimulator/TableHelper.java
@@ -0,0 +1,37 @@
+package RISCVSimulator;
+
+import javafx.beans.property.SimpleStringProperty;
+
+public class TableHelper {
+    private SimpleStringProperty name;
+    private SimpleStringProperty value;
+
+    public TableHelper(String name, String value){
+        this.name = new SimpleStringProperty(name);
+        this.value = new SimpleStringProperty(value);
+    }
+
+    public String getName(){
+        return name.get();
+    }
+
+    public String getValue(){
+        return value.get();
+    }
+
+    public void setName(String name){
+        this.name.set(name);
+    }
+
+    public void setValue(String value){
+        this.value.set(value);
+    }
+
+    public SimpleStringProperty nameProperty(){
+        return name;
+    }
+
+    public SimpleStringProperty valueProperty(){
+        return value;
+    }
+}
